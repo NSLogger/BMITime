5326bad0977c515ca1cdc3f276f0e7d141d3a938
Arun Agrawal <arunagw@gmail.com>
2013-10-15 22:33:06 +0200

    Initial Commit

---
+++ BMITime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BMITime
+//
+//  Created by Arun Agrawal on 15/10/13.
+//  Copyright (c) 2013 Arun Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
