c279e4f81c3a8be4ebdc285b1441a9ed14421581
Arun Agrawal <arunagw@gmail.com>
2013-10-18 15:57:29 +0200

    Assets and employees

--- BMITime/main.m
+++ BMITime/main.m
@@ -7,24 +7,75 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Person.h"
+#import "Employee.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
+        /// create an array if Employee objects
+        NSMutableArray *employees = [[NSMutableArray alloc] init];
         
-        Person *person = [[Person alloc] init];
+        for (int i = 0; i < 10; i++) {
+            // create an instance of Employee
+            
+            Employee *person = [[Employee alloc] init];
+            
+            [person setWeightInKilos:90 + i];
+            [person setHeightInMeters:1.8 - i/10.0];
+            [person setEmployeeID:i];
+
+            // Put the employee in the employees array
+            
+            [employees addObject:person];
+            
+        }
+       
+        NSMutableArray *allAssets = [[NSMutableArray alloc] init];
+        // Create 10 assts
+        for (int i = 0; i < 10; i++) {
+            
+            // Create an asset
+            
+            Asset *asset = [[Asset alloc] init];
+            
+            // Give it some label
+            
+            NSString *currentLabel = [NSString stringWithFormat:@"Laptop %d", i];
+            
+            [asset setLabel:currentLabel];
+            [asset setResaleValue:i * 17];
         
-        [person setWeightInKilos:96];
-        [person setHeightInMeters:1.8];
+            // Get a randome number between 0 and 9
+            NSUInteger randomIndex = random() % [employees count];
+            
+            // Find that employee
+            
+            Employee *randomEmployee = [employees objectAtIndex:randomIndex];
+            
+            // Assing the asset to the employee
+            
+            [randomEmployee addAssetsObject:asset];
+            
+            [allAssets addObject:asset];
+            
+        }
         
-        float bmi = [person bodyMassIndex];
-        NSLog(@"person (%d, %f) has a BMI of %f",
-              [person weightInKilos], [person heightInMeters], bmi);
+        NSLog(@"Employees %@", employees);
+        NSLog(@"Giving up ownership to employee");
+        
+        [employees removeObjectAtIndex:5];
+        
+        NSLog(@"all assets %@", allAssets);
+        
+        NSLog(@"Giving up ownership to array");
+        
+        allAssets = nil;
+        employees = nil;
+
         
     }
+    sleep(100);
     return 0;
 }
-
